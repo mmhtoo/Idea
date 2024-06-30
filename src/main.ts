768e2f26d39acd58403ee3cae285f731a4694cd3
myominhtoo <wer.jp.myomin.htoo@gmail.com>
2024-06-30 17:30:30 +0630

    config: tanstack query and virtual

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,12 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import { VueQueryPlugin } from '@tanstack/vue-query'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(VueQueryPlugin)
 
 app.mount('#app')
